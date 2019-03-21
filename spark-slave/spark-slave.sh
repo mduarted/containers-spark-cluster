cb8192049743bdd32242a880184b80b73706dd37
matheus <matheusdias0211@gmail.com>
2019-03-21 15:23:53 -0300

    Including spark cores and memory parameter for slaves

--- spark-slave/spark-slave.sh
+++ spark-slave/spark-slave.sh
@@ -2,4 +2,4 @@
 
 export SPARK_HOME="/spark"
 
-/spark/bin/spark-class org.apache.spark.deploy.worker.Worker $SPARK_MASTER_HOST
+/spark/bin/spark-class org.apache.spark.deploy.worker.Worker $SPARK_MASTER_HOST -c $SPARK_EXECUTION_CORES -m $SPARK_EXECUTION_MEM
